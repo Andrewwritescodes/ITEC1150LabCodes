f9d7708d7077e23460b1d3c936201e2ed9b5343d
Andrew O'Keefe <aokeefe90@yahoo.com>
2024-09-22 19:23:19 -0500

    Add files via upload

---
+++ january_calendar.py
@@ -0,0 +1,4 @@
+# Question 1: For loops - January Calendar
+
+for date in range (1, 32):  # Date range will be from 1 to 31
+    print(f'January {date}')    # Date number will print after word "January"
