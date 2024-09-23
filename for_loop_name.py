85805252d10acbc8676fcc00c1790a96f2a830d8
Andrew O'Keefe <aokeefe90@yahoo.com>
2024-09-22 19:38:52 -0500

    Delete for_loop_name.py

--- for_loop_name.py
+++
@@ -1,6 +0,0 @@
-# Question 2: For loops - shout your name vertically
-
-name = input('Please enter your name: ') # User will be asked to input name
-
-for letter in name: # For loop to print name as entered
-    print(letter)
