1515919b144108c5c75379b3f18dfc13ebd3cebf
Andrew O'Keefe <aokeefe90@yahoo.com>
2024-09-22 19:39:05 -0500

    Delete january_calendar.py

--- january_calendar.py
+++
@@ -1,4 +0,0 @@
-# Question 1: For loops - January Calendar
-
-for date in range (1, 32):  # Date range will be from 1 to 31
-    print(f'January {date}')    # Date number will print after word "January"
