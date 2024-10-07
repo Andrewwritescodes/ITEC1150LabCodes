2f97b889503fb37cdf389583e3234494c2b177a4
Andrew O'Keefe <aokeefe90@yahoo.com>
2024-10-06 21:13:26 -0500

    Add files via upload

---
+++ email.py
@@ -0,0 +1,5 @@
+email = 'ANDREW.OKEEFE@GO.MINNEAPOLIS.EDU'   # Example email address written in non-standard uppercase
+
+lowercase_email = email.lower()  # This will convert the email address to the lowercase standard
+
+print(f'Your email address is: {lowercase_email}')
