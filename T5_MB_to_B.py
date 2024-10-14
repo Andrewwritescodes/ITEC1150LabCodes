82bf4d9fc1d17bb6bf6ffe9547cd8a079881a458
Andrew O'Keefe <aokeefe90@yahoo.com>
2024-10-13 19:26:59 -0500

    Add files via upload

--- T5_MB_to_B.py
+++ T5_MB_to_B.py
@@ -1,10 +1,10 @@
 def main():
-    Megabytes = float(input('Please enter number of Megabytes: '))
-    Bytes = Megabytes_to_Bytes(Megabytes)
-    print(f'{Megabytes} Megabyte(s) is equal to {Bytes} Bytes')
+    Megabytes = float(input('Please enter number of Megabytes: '))  # First line that will print. This will ask user for input in MB
+    Bytes = Megabytes_to_Bytes(Megabytes)   # Defines the variable "Bytes" with the next function
+    print(f'{Megabytes} Megabyte(s) is equal to {Bytes} Bytes') # Final printed line of the program's output
 
-def Megabytes_to_Bytes(Megabytes):
-    Bytes = Megabytes * 1000000
+def Megabytes_to_Bytes(Megabytes):  # Function needed to define the Bytes variable in the main() function
+    Bytes = Megabytes * 1000000 # Calculates MB to B conversion
     return Bytes
 
 main()
